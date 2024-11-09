5a1909659b7f5eea5085147084980a1a40de0ed0
xoooov <88070121+findmover@users.noreply.github.com>
2024-11-09 14:43:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ env_cookies = os.getenv('WXREAD_COOKIES')
 env_num = os.getenv('READ_NUM')
 env_method = os.getenv('PUSH_METHOD')
 
-headers = json.loads(env_headers) if env_headers else local_headers
-cookies = json.loads(env_cookies) if env_cookies else local_cookies
+headers = json.loads(json.dumps(eval(env_headers))) if env_headers else local_headers
+cookies = json.loads(json.dumps(eval(env_cookies))) if env_cookies else local_cookies
 number = int(env_num) if env_num not in (None, '') else 120
 
 
