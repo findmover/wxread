866a931615bf3bc83ba88bf0f937a689a3982624
v122833 <arcpan@qq.com>
2025-03-24 13:23:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ logging.info("🎉 阅读脚本已完成！")
 
 if PUSH_METHOD not in (None, ''):
     logging.info("⏱️ 开始推送...")
-    push(f"🎉 微信读书自动阅读完成！\n ⏱️ 阅读时长：{(index - 1) * 0.5}分钟。", PUSH_METHOD)
+    push(f"微信读书打卡！时长：{(index - 1) * 0.5}min。", PUSH_METHOD)
