c87ef0f41b4588f3d71eba461b699b0cc521fafd
Johnny Chen <cjohnny94@gmail.com>
2025-04-09 20:47:49 +0930

    feat: update cookies, headers and data.

--- config.py
+++ config.py
@@ -23,41 +23,58 @@ curl_str = os.getenv('WXREAD_CURL_BASH')
 
 # headers、cookies是一个省略模版，本地或者docker部署时对应替换
 cookies = {
-    'RK': 'oxEY1bTnXf',
-    'ptcz': '53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b',
-    'pac_uid': '0_e63870bcecc18',
-    'iip': '0',
-    '_qimei_uuid42': '183070d3135100ee797b08bc922054dc3062834291',
-    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FeEOpSbFh2Mb1bUxMW9Y3FRPfXwWvOLaNlsjWIkcKeeNg6vlVS5kOVuhNKGQ1M8zaggLqMPmpE5qIUdqEXlQgYg%2F132',
-    'wr_gender': '0',
+    'wr_gid': '292311143',
+    'wr_vid': '18916453',
+    'wr_rt': 'web%40TSjdRuJBTm6xn572iXC_AL',
+    'wr_localvid': '549320307120a46554968f5',
+    'wr_name': 'Johnny',
+    'wr_gender': '1',
+    'wr_theme': 'dark',
+    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FT56yGfB99fRO6J8KxRG7I68EuWibrPiav8RhCicYjaGwc5uasWhacGpNAfCNeK1B5sV1EvpJZ6WkWXXJndXdV8saw%2F132',
+    'wr_pf': 'NaN',
+    'wr_skey': 'hAKwacx6',
+    'wr_fp': '1469692579',
+    'mp_49719c4dbff298b1e6c9adca527948b5_mixpanel': '%7B%22distinct_id%22%3A%20%22%24device%3A195db96d3ce75c-0fd623595e5fb5-1b525636-384000-195db96d3cf75d%22%2C%22%24device_id%22%3A%20%22195db96d3ce75c-0fd623595e5fb5-1b525636-384000-195db96d3cf75d%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%7D',
 }
 
 headers = {
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5',
-    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+    'accept-language': 'en-AU,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'baggage': 'sentry-environment=production,sentry-release=dev-1743523268690,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=e77196018bde478180011b6135742d06',
+    'content-type': 'application/json;charset=UTF-8',
+    'origin': 'https://weread.qq.com',
+    'priority': 'u=1, i',
+    'referer': 'https://weread.qq.com/web/reader/ce032b305a9bc1ce0b0dd2ak1c932da029c1c9ac015999a',
+    'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sentry-trace': 'e77196018bde478180011b6135742d06-b6d750f0f6cfc313',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+    # 'cookie': 'wr_gid=292311143; wr_vid=18916453; wr_rt=web%40TSjdRuJBTm6xn572iXC_AL; wr_localvid=549320307120a46554968f5; wr_name=Johnny; wr_gender=1; wr_theme=dark; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FT56yGfB99fRO6J8KxRG7I68EuWibrPiav8RhCicYjaGwc5uasWhacGpNAfCNeK1B5sV1EvpJZ6WkWXXJndXdV8saw%2F132; wr_pf=NaN; wr_skey=hAKwacx6; wr_fp=1469692579; mp_49719c4dbff298b1e6c9adca527948b5_mixpanel=%7B%22distinct_id%22%3A%20%22%24device%3A195db96d3ce75c-0fd623595e5fb5-1b525636-384000-195db96d3cf75d%22%2C%22%24device_id%22%3A%20%22195db96d3ce75c-0fd623595e5fb5-1b525636-384000-195db96d3cf75d%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22__mps%22%3A%20%7B%7D%2C%22__mpso%22%3A%20%7B%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%7D%2C%22__mpus%22%3A%20%7B%7D%2C%22__mpa%22%3A%20%7B%7D%2C%22__mpu%22%3A%20%7B%7D%2C%22__mpr%22%3A%20%5B%5D%2C%22__mpap%22%3A%20%5B%5D%7D',
 }
-
 
 """
 建议保留区域|默认读三体，其它书籍自行测试时间是否增加
 """
 data = {
-    "appId": "wb182564874663h152492176",
-    "b": "ce032b305a9bc1ce0b0dd2a",
-    "c": "7cb321502467cbbc409e62d",
-    "ci": 70,
-    "co": 0,
-    "sm": "[插图]第三部广播纪元7年，程心艾AA说",
-    "pr": 74,
-    "rt": 30,
-    "ts": 1727660516749,
-    "rn": 31,
-    "sg": "991118cc229871a5442993ecb08b5d2844d7f001dbad9a9bc7b2ecf73dc8db7e",
-    "ct": 1727660516,
-    "ps": "b1d32a307a4c3259g016b67",
-    "pc": "080327b07a4c3259g018787",
+    'appId': 'wb115321887466h725297694',
+    'b': 'ce032b305a9bc1ce0b0dd2a',
+    'c': '1c932da029c1c9ac015999a',
+    'ci': 71,
+    'co': 389,
+    'sm': '危机纪元4年云天明今天张医生来病房查诊，',
+    'pr': 74,
+    'rt': 5,
+    'ts': 1744197384707,
+    'rn': 776,
+    'sg': '29e4c427e1d2675ba9913c245e1e2b2c75f664019ad9a5d1bf061602484d02b5',
+    'ct': 1744197384,
+    'ps': 'cdc320407a656f19g017cbf',
+    'pc': '5ba32ed07a656f19g0187e1',
+    's': '8501ecb4',
 }
 
 
